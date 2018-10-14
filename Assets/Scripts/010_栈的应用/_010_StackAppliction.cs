41f90ee2280241480f66dfa35b937fbc3e9b09f8
lijian <1287053830@qq.com>
2018-10-14 12:55:30 +0800

    队列的应用，键盘缓冲

--- Assets/Scripts/010_栈的应用/_010_StackAppliction.cs
+++ Assets/Scripts/010_栈的应用/_010_StackAppliction.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-    
+
 /// <summary>
 /// 栈的应用
 /// </summary>
@@ -15,11 +16,9 @@ public class StackAppliction
     /// <returns></returns>
     public bool BracketMatch(string str)
     {
-        Debug.Log(str);
-
-        if (str == string.Empty||str==null)//校验字符括号是否为空
+        if (str == string.Empty || str == null)//校验字符括号是否为空
         {
-            Debug.Log("字符括号为空！匹配失败！");
+            Debug.Log("字符括号为空！括号匹配失败！");
             return false;
         }
 
@@ -40,7 +39,7 @@ public class StackAppliction
                 case '}':
                     if (stack.IsEmpty())//使用系统提供的栈：stack.Count==0
                     {
-                        Debug.Log("右括号   " + str[i] + "  多余！匹配失败！");
+                        Debug.Log("右括号   " + str[i] + "  多余！括号匹配失败！");
                         return false;
                     }
                     else
@@ -53,24 +52,23 @@ public class StackAppliction
                         }
                         else
                         {
-                            Debug.Log("左括号:  " + left + "  与右括号：   " + str[i] + "   不同类型！匹配失败！ ");
+                            Debug.Log("左括号:  " + left + "  与右括号：   " + str[i] + "   不同类型！括号匹配失败！ ");
                             return false;
                         }
                     }
                     break;
                 default:
-                    Debug.Log(str[i] + "   不合法！匹配失败！ ");
-                    return false;
+                    break;
             }
         }
 
         if (stack.IsEmpty())//使用系统提供的栈：stack.Count==0
         {
-            Debug.Log("匹配成功！");
+            Debug.Log("括号匹配成功！");
             return true;
         }
-        
-        Debug.Log("左括号:  " + stack.Peek() + "   多余！匹配失败！");
+
+        Debug.Log("左括号:  " + stack.Peek() + "   多余！括号匹配失败！");
         return false;
 
     }
@@ -101,10 +99,215 @@ public class StackAppliction
 
     #endregion
 
-    
+    #region 2、表达式求值:  "5 * ( ( 3 + 7 )+(3*2)^2 ) - 4 / 2"
+    //2、表达式求值：
+    public int ExpEvaluation(string middleExp)
+    {
+
+
+        //1、将str 中缀表达式转为后缀表达式
+        string TrailExp = ToTrailExp(middleExp);
+
+
+        //2、对后缀表达式进行求值操作
+        return TrailExpEvalution(TrailExp);
+
+    }
+
+    /// <summary>
+    /// 中缀表达式转为后缀表达式
+    /// </summary>
+    /// <param name="str">中缀表达式</param>
+    /// <returns>后缀表达式</returns>
+    public string ToTrailExp(string str)
+    {
+        string traiExp = null;
+
+        if (str == null || str == string.Empty)//校验str是否空
+        {
+            return null;
+        }
+
+
+        if (!BracketMatch(str))//判断是否平衡圆括号
+        {
+            Debug.Log("表达式的圆括号不平衡，表达式有误！");
+            return null;
+        }
+
+        Stack<char> optrStack = new Stack<char>(); //操作符栈
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            switch (str[i])
+            {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    traiExp += str[i];
+                    break;
+                case ' ':
+                    break;
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '^':
+                    if (optrStack.Count == 0)
+                    {
+                        optrStack.Push(str[i]);
+                    }
+                    else
+                    {
+                        char op = optrStack.Peek();
+                        if (op == '(')
+                        {
+                            optrStack.Push(str[i]);
+                        }
+                        else
+                        {
+                            if (Priority(str[i]) <= Priority(op))
+                            {
+                                traiExp += optrStack.Pop();
+                                optrStack.Push(str[i]);
+                            }
+                            else
+                            {
+                                optrStack.Push(str[i]);
+                            }
+                        }
+                    }
+                    break;
+                case '('://碰到左括号，直接入栈
+                    optrStack.Push(str[i]);
+                    break;
+                case ')': //一直出栈至首次出栈的"("
+                    while (optrStack.Count > 0 && optrStack.Peek() != '(')
+                    {
+                        traiExp += optrStack.Pop();
+                    }
+                    if (optrStack.Count != 0)
+                    {
+                        optrStack.Pop();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        while (optrStack.Count != 0) //把剩余操作符出栈并输出
+        {
+            traiExp += optrStack.Pop();
+        }
+
+        return traiExp;
+
+    }
+
+    /// <summary>
+    /// 判断操作符的优先级
+    /// </summary>
+    /// <param name="c"></param>
+    /// <returns></returns>
+    private int Priority(char c)
+    {
+        switch (c)
+        {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+            default:
+                return -1;
+        }
+    }
+
+
+    /// <summary>
+    /// 对后缀表达式进行求值操作
+    /// </summary>
+    /// <param name="trailExp">后缀表达式</param>
+    /// <returns>结果</returns>
+    public int TrailExpEvalution(string trailExp)
+    {
+        if (trailExp == null || trailExp == string.Empty) //校验是否空
+        {
+            return -1;
+        }
+
+        Stack<int> numStack = new Stack<int>();
+
+        for (int i = 0; i < trailExp.Length; i++)
+        {
+            int n1, n2;
+
+            switch (trailExp[i])
+            {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    numStack.Push(int.Parse(trailExp[i].ToString()));
+                    break;
+                case ' ':
+                    break;
+                case '+':
+                    n1 = numStack.Pop();
+                    n2 = numStack.Pop();
+                    numStack.Push(n1 + n2);
+                    break;
+                case '-':
+                    n1 = numStack.Pop();//减数
+                    n2 = numStack.Pop();//被减数
+                    numStack.Push(n2 - n1);
+                    break;
+                case '*':
+                    n1 = numStack.Pop();
+                    n2 = numStack.Pop();
+                    numStack.Push(n2 * n1);
+                    break;
+                case '/':
+                    n1 = numStack.Pop();//除数
+                    n2 = numStack.Pop();//被除数
+                    numStack.Push(n2 / n1);
+                    break;
+                case '^':
+                    n1 = numStack.Pop();//幂数
+                    n2 = numStack.Pop();//低数
+                    numStack.Push((int)Math.Pow(n2, n1));
+                    break;
+                default:
+                    break;
+            }
+        }
+        return numStack.Pop();
+    }
+
+    #endregion
+
+
 
 
 }
+
 
 
 public class _010_StackAppliction : MonoBehaviour
@@ -116,7 +319,36 @@ public class _010_StackAppliction : MonoBehaviour
 
         StackAppliction appliction = new StackAppliction();
 
+        //print(appliction.BracketMatch(null));
+
+        //print(appliction.BracketMatch(""));
+
+        //print(appliction.BracketMatch("({})"));
+
+        //print(appliction.BracketMatch("({fsd[f}af)"));
+
+        //print(appliction.BracketMatch("(af{af}"));
+
+        //print(appliction.BracketMatch("af{af}sf)af"));
+
+        //print(appliction.BracketMatch("(dfgd{q)"));
+
+        //print("判断是否平衡圆括号---------------");
+
+        //print("是否平衡圆括号:" + appliction.BracketMatch("()()"));
+        //print("是否平衡圆括号:" + appliction.BracketMatch("())"));
+        //print("是否平衡圆括号:" + appliction.BracketMatch("(((((())))))"));
+
+        string str = "5 * ( ( 3 + 7 )+(3*2)^2 ) - 4 / 2";
+
+        print("中缀表达式： " + str);
+
+        print("后缀表达式： " + appliction.ToTrailExp(str));
+
+        print("表达式结果： " + appliction.ExpEvaluation(str));
+
 
 
     }
 }
+
---
+++ Assets/Scripts/011_队列应用/_011_QueueApplication.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _011_QueueApplication : MonoBehaviour
+{
+    SeqQueue<string> queue = new SeqQueue<string>(10);
+    string input = null, output = null;
+    
+
+    void Start()
+    {
+
+        //for (; ; )
+        //{
+        //    for (; ; )
+        //    {
+        //        print("进程1执行中，输入缓冲中！");
+        //        if (Input.anyKeyDown)
+        //        {
+        //            input = GetChar();
+
+        //            if (input == ";" || input == ",") //正常结束缓冲输入进程
+        //            {
+        //                break;
+        //            }
+
+        //            queue.Enqueue(input); //加入缓冲队列
+
+        //            if (queue.IsFull()) //缓冲循环队列输入已满
+        //            {
+        //                print("缓冲循环队列输入已满！");
+        //                break;
+        //            }
+        //        }
+        //    }
+
+
+        //}
+
+
+    }
+
+    private void Update()
+    {
+        print("等待输入！");
+        InputString();
+
+
+
+    }
+
+    
+
+
+    public void InputString()
+    {
+        if (Input.anyKeyDown)
+        {
+            input = Input.inputString;
+
+            if (queue.IsFull()) //缓冲循环队列输入已满
+            {
+                print("缓冲循环队列输入已满！结束缓冲输入进程");
+                OutputString();
+                return;
+            }
+
+            queue.Enqueue(input); //加入缓冲队列
+
+            if (input == ";" || input == ",") //正常结束缓冲输入进程
+            {
+                print("遇到  ;  ,  字符正常结束缓冲输入进程");
+                OutputString();
+            }
+
+            if(input == ".")
+            {
+                print("程序终止");
+                UnityEditor.EditorApplication.isPlaying = false;
+                OutputString();
+            }
+        }
+    }
+
+
+    public void OutputString()
+    {
+        while(!queue.IsEmpty())
+        {
+            output += queue.Dequeue();
+        }
+        print("显示缓冲输入区的字符： " + output);
+    }
+
+    
+}
